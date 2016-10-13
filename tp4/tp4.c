37c4e9d1e21e34a6254a7b8f4c6a05ec423d549a
Frederic PROCHNOW <frederic.prochnow@etudiant.univ-lille1.fr>
2016-10-13 10:59:16 +0200

    1310161059

--- tp4/tp4.c
+++ tp4/tp4.c
@@ -34,16 +34,33 @@ char * saisie ()
 
 }
 
-int main(void)
+int main(int argc,char *argv[])
 {
-  char *s = "fred";
-  char *r = "derf";
-  char *t = miroir(s);
-  if(*r == *t) {
-    printf("c'est ok \n");
-  } 
-  else {
-    printf("ca marche pas \n");
+  int i;
+  int j;
+  int im = 0;
+  int is = 0;
+  int res = 0;
+  if(argc <= 1) {
+    printf("\nLe Programme n'a reçu aucun argument\n");
+  } else {
+     for(i = 1; i< argc; i++) {
+       printf("%d:%s\n",i,argv[i]);
+       if(argv[i] [0] == '-') {
+         for(j=1; j<strlen(argv[i]); j++) {
+            switch(argv[i] [j]) {
+               case 'm' : im = 1; break;
+               case 's' : is = 1; break;
+               default : res = -1; break;
+            }
+         }
+       } else {
+          if(im == 1) printf("%s\n",miroir(argv[i]));
+          else if(is == 1) printf("%s\n",saisie());
+          else if (res == -1) printf("\n ERREUR !!!\n");
+          else printf("\n ERREUR !!!\n");
+       }
+    }
   }
   return 1;
 }
