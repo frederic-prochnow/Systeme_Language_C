8704cdceec43bd1c8a1a0574237f3fe089d26103
Frederic PROCHNOW <frederic.prochnow@etudiant.univ-lille1.fr>
2016-11-03 11:56:59 +0100

    0310161156 tp4

--- tp4/tp4.c
+++ tp4/tp4.c
@@ -7,13 +7,15 @@
 char * miroir (const char *s)
 {
   char *u = malloc(strlen(s)*sizeof(char));
-  if(u == "null") {
-     return "null";
+  if(u == NULL) {
+     printf("ERREUR MIROIR /n");
+     return NULL;
   } else {
     int i = 0;
-    while (i<strlen(s))
+    int len = strlen(s);
+    while (i<len)
     {
-      u[i] = s[strlen(s)-i-1];
+      u[i] = s[len-i-1];
       i++; 
     }
     return u;
@@ -38,41 +40,47 @@ int main(int argc,char *argv[])
 {
   int i;
   int j;
+  int len;
   int im = 0;
   int is = 0;
   int res = 0;
+  char erreur;
   if(argc <= 1) {
     printf("\nLe Programme n'a recu aucun argument\n");
   } else {
      for(i = 1; i< argc; i++) {
        if(argv[i] [0] == '-') {
-         for(j=1; j<strlen(argv[i]); j++) {
+          len = strlen(argv[i]);
+          for(j=1; j<len; j++) {
             switch(argv[i] [j]) {
                case 'm' : im = 1; break;
                case 's' : is = 1; break;
-               default : res = -1; break;
+               default  :res = -1 ;erreur = argv[i][j]; break;
             }
           }
-       }
-       else {
-	 if(    im == 1 && is == 0){
+          if(res != -1 && i<argc && im == 1 && is == 0 )
+            printf ("ERREUR MIROIR\n");
+          if(res != -1 && im == 0 && is == 1 ) {
+            printf("%s\n",saisie());
+            is--;
+          }
+          if(res != -1 && i<argc && im == 1 && is == 1 ) {
+            char * res = saisie();
+            printf("%s%s\n",res,miroir(res));
+            im--; is--;
+          }
+          if( res == -1) {
+            printf ("-%c >> ERREUR ARGUMENT(S)\n",erreur);
+          }
+
+         }else {
+	  if(res != -1 && im == 1 && is == 0){
 	   printf("%s\n",miroir(argv[i])); 
 	   im--;
 	 }
-	  
-	 else if(im == 0 && is == 1) {
-	   printf("%s\n",saisie());
-	   is--;
-	 }
-	 else if(im == 1 && is == 1){
-	   char * res = saisie();
-	   printf("%s%s\n",res,miroir(res));
-	   im--;
-	   is--;
-	 }
-	 else if (res == -1) printf("\n ERREUR !!!\n");
-	 else printf("\n ERREUR !!!\n");
-       }
+	  if (res == -1) printf("\n ERREUR !!!\n");
+        }
      }
   }
+  return 0;
 }
