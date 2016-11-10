317eb212680d01d7a4a55b80a689355fbd0a896d
Frederic PROCHNOW <frederic.prochnow@etudiant.univ-lille1.fr>
2016-11-10 11:56:10 +0100

    1011161156 tp6

--- tp6/tp6.c
+++ tp6/tp6.c
@@ -16,7 +16,7 @@ int lire_deux_octets(int openFile, uint16 *val) {
   int x = buffer[0] <<8;
   int y = buffer[1];
   val = x+y;
-  return val;
+  return *openFile;
 }
 
 /*
@@ -32,12 +32,14 @@ int lire_quatre_octets(int openFile, uint32 *val) {
   int y = buffer[2] <<8;
   int z = buffer[3];
   val = w+x+y+z;
-  return val;
+  return *openFile;
 }
 
 /*
 lit l’entete du fichier en entree et stocke les informations dans la structure passee en parametre en se servant des fonctions precedentes. La fonction doit retourner -1 si l’entete n’a pas pu etre lue correctement
 */
 int lire_entete(int de, entete_bmp *entete) {
+  entete->taille_entete = ...;
+  // a finir
   return 0;
 }
